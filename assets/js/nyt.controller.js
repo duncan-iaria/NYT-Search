ef41db583d93d65f04e9130f6f752728f00e51da
SNOODBOOK\MiniSnood <duncaniaria@gmail.com>
2017-06-03 12:26:48 -0400

    initial project set up

---
+++ assets/js/nyt.controller.js
@@ -0,0 +1,2 @@
+//this will handle updating the views with the appropriate data
+//this will pass in search fields and put them in the right spots
---
+++ assets/js/nyt.service.js
@@ -0,0 +1,3 @@
+//this will serve the data to the controller
+//(do the actual api call and return an object)
+var apiKey = '25e3b019f8b04fa6ab435adcd6e48286';
